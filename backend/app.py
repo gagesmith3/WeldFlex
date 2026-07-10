00f63a7bb36196f6ec549f738044074464a5eee1
Gage M. Smith <60896044+gagesmith3@users.noreply.github.com>
2026-07-10 09:24:38 -0400

    app fix

--- backend/app.py
+++ backend/app.py
@@ -1026,4 +1026,4 @@ def ui_settings_shutdown():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
